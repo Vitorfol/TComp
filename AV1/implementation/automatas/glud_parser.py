df76e683c8b3da37abcd6b8455706299ab65c1b1
iandomingos777 <ianbrunodomingos@gmail.com>
2025-05-26 19:00:11 -0300

    criação do glud_parser
    criação da função responsável por converter GLUD em AF.

---
+++ AV1/implementation/automatas/glud_parser.py
@@ -0,0 +1,38 @@
+from implementation.automatas.af import Automata
+
+def glud_parser(glud):
+    states= set(glud['variables'])
+    alphabet = set(glud['terminals'])
+    initial_state = glud['initial_state']
+    final_states = set()
+    transitions = dict()
+
+    final_states.add('F')
+    for prod in glud['productions']:
+        esquerda, direita = prod.split('->')
+        esquerda = esquerda.strip()
+        direita = direita.strip()
+       
+        if direita == 'ε':
+            simbolo = 'ε'
+            destino = 'F'
+        elif len(direita) == 1:
+            simbolo = direita
+            destino = 'F'
+        elif len(direita) == 2:
+            simbolo = direita[0]
+            destino = direita[1]
+        else:
+            continue
+        if esquerda not in transitions:
+            transitions[esquerda] = dict()
+        if simbolo not in transitions[esquerda]:
+            transitions[esquerda][simbolo] = set()
+        
+        transitions[esquerda][simbolo].add(destino)
+
+
+    return Automata(states, alphabet, transitions, initial_state, final_states)
+
+
+
