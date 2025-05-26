ed5fadc72cb969dc19287e129fc520fce945f7fe
Vitor Fontenele <vitorfol2302@gmail.com>
2025-05-26 18:03:16 -0300

    Determinized iniciado

---
+++ AV1/implementation/algorithms/determinization.py
@@ -0,0 +1,17 @@
+from implementation.automatas.af import Automata
+
+def determinize(Automata): 
+    alphabet = Automata.alphabet
+    initial_state = eclose(Atuomata.initial_state, Automata.transition_function)
+    state_set = set()
+    state_set.add(initial_state)
+    transiction_function = {}
+
+    #for i in state_set: 
+        # TODO
+    
+        
+
+
+# def eclose(state, transition_function):
+    # TODO
