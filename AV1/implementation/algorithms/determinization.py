07ea7a38f66aa1ef6418ade7b7be93aef7b57623
iandomingos777 <ianbrunodomingos@gmail.com>
2025-05-29 20:57:22 -0300

    criação da função eclose
    criação da função responsável por retornar o eclose dos estados.

--- AV1/implementation/algorithms/determinization.py
+++ AV1/implementation/algorithms/determinization.py
@@ -2,16 +2,29 @@ from implementation.automatas.af import Automata
 
 def determinize(Automata): 
     alphabet = Automata.alphabet
-    initial_state = eclose(Atuomata.initial_state, Automata.transition_function)
+    initial_state = eclose(Automata.initial_state, Automata.transition_function)
     state_set = set()
     state_set.add(initial_state)
     transiction_function = {}
 
-    #for i in state_set: 
-        # TODO
+    for i in Automata.state_set: 
+        state_set.add(eclose(i, Automata.transition_function))
+    for i in state_set:
+        transiction_function.add(transictions(i, Automata.transition_function))
     
         
 
 
-# def eclose(state, transition_function):
-    # TODO
+def eclose(state, transition_function):
+    closure = set()
+    stack = [state]
+
+    while stack:
+        current = stack.pop()
+        if current not in closure:
+            closure.add(current)
+            for next_state in transition_function.get(current, {}).get('ε', set()):
+                stack.append(next_state)
+
+    return closure
+      
