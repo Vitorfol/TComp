3f205cf03b972bb982fedf9072010d01028c242f
iandomingos777 <ianbrunodomingos@gmail.com>
2025-05-31 22:20:40 -0300

    criação da função complement
    criação da função responsável pela operação de complemento.

---
+++ AV1/implementation/algorithms/complement.py
@@ -0,0 +1,14 @@
+from implementation.automatas.af import Automata
+
+def complement(automata: Automata):
+    alphabet = automata.alphabet
+    initial_state = automata.initial_state
+    transitions = automata.transition_function
+    states = automata.state_set
+
+    final_states = set()
+    for state in states:
+        if state not in automata.final_states:
+            final_states.add(state)
+
+    return Automata(states, alphabet, transitions, initial_state, final_states, deterministic=True)
